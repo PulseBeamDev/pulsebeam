860af39962702fb9d0c407e8562cf3e7ab4e94fe
Lukas Herman <lherman.cs@gmail.com>
2025-08-27 15:03:08 -0400

    add a simple proxy to UDP interface

--- pulsebeam-runtime/src/net.rs
+++ pulsebeam-runtime/src/net.rs
@@ -1,10 +1,10 @@
-use std::io::{self, ErrorKind};
+use std::io;
 use std::net::SocketAddr;
 use std::sync::Arc;
 
 use bytes::Bytes;
 use tokio::net::UdpSocket;
-use tokio::sync::{Mutex, mpsc};
+use tokio::sync::mpsc;
 
 #[derive(Clone, Copy, Debug)]
 pub enum Transport {
@@ -16,42 +16,17 @@ pub enum Transport {
 // ======================== UDP ========================
 pub struct UdpSocketImpl {
     socket: Arc<UdpSocket>,
-    incoming: Mutex<mpsc::Receiver<(SocketAddr, Bytes)>>,
 }
 
 impl UdpSocketImpl {
     pub async fn new(local_addr: SocketAddr) -> io::Result<Self> {
         let socket = Arc::new(UdpSocket::bind(local_addr).await?);
-        let (tx, rx) = mpsc::channel(128);
-        let recv_socket = socket.clone();
 
-        tokio::spawn(async move {
-            let mut buf = vec![0u8; 65535];
-            loop {
-                match recv_socket.recv_from(&mut buf).await {
-                    Ok((len, addr)) => {
-                        let _ = tx.send((addr, Bytes::copy_from_slice(&buf[..len]))).await;
-                    }
-                    Err(e) => eprintln!("UDP recv error: {}", e),
-                }
-            }
-        });
-
-        Ok(Self {
-            socket,
-            incoming: Mutex::new(rx),
-        })
+        Ok(Self { socket })
     }
 
     pub async fn recv_from(&self, buf: &mut [u8]) -> io::Result<(usize, SocketAddr)> {
-        let mut rx = self.incoming.lock().await;
-        let (addr, data) = rx
-            .recv()
-            .await
-            .ok_or_else(|| io::Error::new(ErrorKind::BrokenPipe, "Receive channel closed"))?;
-        let len = data.len().min(buf.len());
-        buf[..len].copy_from_slice(&data[..len]);
-        Ok((len, addr))
+        self.socket.recv_from(buf).await
     }
 
     pub async fn send_to(&self, buf: &[u8], addr: SocketAddr) -> io::Result<usize> {
