c8b148f561d4de1cd08250e699b4dc760da57397
Lukas Herman <lherman.cs@gmail.com>
2025-09-10 16:54:28 -0400

    chore: move build.rs

--- pulsebeam/build.rs
+++ pulsebeam/build.rs
@@ -1,9 +1,9 @@
 fn main() {
     prost_build::Config::new()
         .out_dir("src/proto")
-        .compile_protos(&["proto/sfu.proto"], &["proto"])
+        .compile_protos(&["../proto/sfu.proto"], &["../proto"])
         .expect("Failed to compile .proto files");
 
     // Tell cargo to rerun if the proto files change
-    println!("cargo:rerun-if-changed=proto/");
+    println!("cargo:rerun-if-changed=..proto/");
 }
