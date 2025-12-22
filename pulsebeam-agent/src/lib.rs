8ddd4e66d1750135c8a12919ac274097b5bf8708
Lukas Herman <lherman.cs@gmail.com>
2025-12-22 13:47:52 -0500

    remove compile check for ffmpeg

--- pulsebeam-agent/src/lib.rs
+++ pulsebeam-agent/src/lib.rs
@@ -1,8 +1,2 @@
-#[cfg(all(feature = "ffmpeg-static", feature = "ffmpeg-dynamic"))]
-compile_error!("Cannot enable both ffmpeg-static and ffmpeg-dynamic features simultaneously");
-
-#[cfg(not(any(feature = "ffmpeg-static", feature = "ffmpeg-dynamic")))]
-compile_error!("Must enable either ffmpeg-static or ffmpeg-dynamic feature");
-
 pub mod agent;
 // pub mod rt;
