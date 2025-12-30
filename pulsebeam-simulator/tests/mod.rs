e45a5d1ebbd3271614e69f62e2eb5e7cce63f271
Lukas Herman <lherman.cs@gmail.com>
2025-12-30 10:40:42 -0500

    init simple proptest

---
+++ pulsebeam-simulator/tests/mod.rs
@@ -0,0 +1,16 @@
+use pulsebeam_simulator::add;
+use test_strategy::{Arbitrary, proptest};
+
+#[derive(Arbitrary, Debug)]
+struct TestInputStruct {
+    #[strategy(1..10u32)]
+    a: u32,
+
+    #[strategy(0..#a)]
+    b: u32,
+}
+
+#[proptest]
+fn my_test(input: TestInputStruct) {
+    assert_eq!(add(input.a, input.b), input.a + input.b);
+}
