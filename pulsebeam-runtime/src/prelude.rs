4eb68817a520c74574b7e56c8efa697596ee7d83
Lukas Herman <lherman.cs@gmail.com>
2026-05-03 17:58:58 +0800

    Revert "update cargo deps"
    This reverts commit 70e4295b9f8737f3c7b81d32dbaeb92c5e142856.

--- pulsebeam-runtime/src/prelude.rs
+++ pulsebeam-runtime/src/prelude.rs
@@ -2,3 +2,4 @@ pub use crate::actor::{Actor, ActorHandle};
 pub use crate::rand::{RngCore, SeedableRng};
 pub use futures_lite::{FutureExt, StreamExt};
 pub use once_cell::sync::Lazy;
+pub use tokio_metrics::TaskMonitor;
