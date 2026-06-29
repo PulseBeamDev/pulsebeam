fc2e075069e8eb8ca1f87df82e024f07a586f1cd
Lukas Herman <lherman.cs@gmail.com>
2026-06-29 14:40:10 -0400

    allow passthrough sigint and add valgrind inject

--- scripts/cmd.sh
+++ scripts/cmd.sh
@@ -1,8 +1,10 @@
 # RUST_LOG=error ./target/release/pulsebeam-cli --api-url http://192.168.4.1:7070 bench --rooms 5 --max-rooms 15 --users-per-room 4 --session-duration 3600 --join-spread-secs 30 --drain-duration 3600 --arrival-rate 5 --fixed-session | tee c-2.csv
 # RUST_LOG=error ./target/release/pulsebeam-cli --api-url http://192.168.4.1:7070 bench --rooms 0 --max-rooms 75 --users-per-room 4 --session-duration 3600 --join-spread-secs 30 --drain-duration 3600 --arrival-rate 1 --fixed-session | tee c-2.csv
-RUST_LOG=error timeout -s SIGINT 600s ./target/release/pulsebeam-cli --api-url http://192.168.4.1:7070 bench \
+#INJECT=valgrind --tool=massif --time-unit=ms
+INJECT=
+RUST_LOG=error timeout --foreground -s SIGINT 600s taskset -c 5-13 ${INJECT} ./target/release/pulsebeam-cli --api-url http://192.168.4.1:7070 bench \
   --users-per-room 4 \
   --arrival-rate 1 \
   --join-spread-secs 5 \
-  --max-rooms 50 \
+  --max-rooms 100 \
   --session-duration 3600
