3e760cee81a49d65d5a436d519bad5ae17cfb7bd
Lukas Herman <lherman.cs@gmail.com>
2026-08-21 09:33:53 -0400

    metrics: add shard-local recorder metrics

--- pulsebeam/src/control/mod.rs
+++ pulsebeam/src/control/mod.rs
@@ -1,10 +1,14 @@
 pub mod api;
 pub mod controller;
 pub(crate) mod core;
+pub(crate) mod lanes;
 mod negotiator;
 mod registry;
 mod room;
 mod router;
+pub(crate) mod state;
+pub(crate) mod stats_aggregator;
+pub(crate) mod subscriptions;
 pub mod tcp_acceptor;
 pub mod ufrag;
 
